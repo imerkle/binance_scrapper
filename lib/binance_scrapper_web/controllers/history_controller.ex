f84547862f335e2bfdc9b835ece60da555c32b83
slim <dsslimshaddy@gmail.com>
2018-09-15 21:42:12 +0530

    volume router add

--- lib/binance_scrapper_web/controllers/history_controller.ex
+++ lib/binance_scrapper_web/controllers/history_controller.ex
@@ -37,13 +37,54 @@ defmodule BinanceScrapperWeb.HistoryController do
           "symbol"=> x["symbol"],
           "price"=>ap,
           "before_price"=> bp,
-          "change"=> (ap - bp) / bp  * 100 
+          "change"=> (ap - bp) / bp  * 100
         }
       end)
       |> Enum.filter(fn x ->  String.contains? x["symbol"], tickers end)
       |> Enum.sort_by(fn(x) -> -x["change"] end)
 
       json(conn, with_changes)
-  	end
+    end
+    
+  	def checkVolume(conn, %{"min" => minutes,"ticker" => ticker}) do
+  		
+      tickers = String.split(ticker, ",")
+
+      {minutes, _} = Integer.parse(minutes)
+      now = DateTime.utc_now |> DateTime.to_unix()
+      
+      timestamp = DateTime.from_unix!(now - minutes * 60 )  |> DateTime.to_naive
+
+      history = BinanceScrapper.History
+      |> where([u], u.inserted_at >= ^timestamp)
+      |> limit(1)
+      |>  BinanceScrapper.Repo.all
+      
+      present = BinanceScrapper.History 
+      |> order_by(desc: :id)
+      |> limit(1)
+      |>  BinanceScrapper.Repo.all
+      
+      volume_before = Poison.decode!(Enum.at(history, 0).volume)
+      volume_after = Poison.decode!(Enum.at(present, 0).volume)
+      ivolume_after = Enum.with_index(volume_after)
+
+      with_changes = 
+      Enum.map(volume_after, fn{x, i} -> 
+        {bv, _} = Float.parse(Enum.at(volume_before,i)["volume"])
+        {av, _} = Float.parse(x["volume"])
+
+        %{
+          "symbol"=> x["symbol"],
+          "volume"=>av,
+          "before_volume"=> bv,
+          "change"=> (av - bv) / bv  * 100
+        }
+      end)
+      |> Enum.filter(fn x ->  String.contains? x["symbol"], tickers end)
+      |> Enum.sort_by(fn(x) -> -x["change"] end)
+
+      json(conn, with_changes)
+  	end    
 
 end
--- lib/binance_scrapper_web/router.ex
+++ lib/binance_scrapper_web/router.ex
@@ -9,6 +9,7 @@ defmodule BinanceScrapperWeb.Router do
     pipe_through :api
 
     get "/check", HistoryController, :check
+    get "/checkv", HistoryController, :checkVolume
 
   end
 end
