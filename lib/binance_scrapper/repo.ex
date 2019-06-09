9e3452cc7d9cf5de3f2f223893a713c455dbe93f
slim <dsslimshaddy@gmail.com>
2019-06-10 04:30:05 +0530

    terraform

--- lib/binance_scrapper/repo.ex
+++ lib/binance_scrapper/repo.ex
@@ -2,4 +2,13 @@ defmodule BinanceScrapper.Repo do
   use Ecto.Repo,
     otp_app: :binance_scrapper,
     adapter: Ecto.Adapters.Postgres
+
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
+  """
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
+
 end
